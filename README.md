2a4e3cc08bf080577500bd3f80af666d61ce84b9
Avielbe <106452396+Avielbe@users.noreply.github.com>
2024-03-11 14:07:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HebCal-v5.1
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Avielbe/HebCal-v5.1)
---
+++ src/Component/styled-components.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+
+
+const StyledParagraph = styled.p`
+  font-family: 'Your Selected Font Name', sans-serif;
+`;
